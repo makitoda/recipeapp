173e552467d688205f8259737406a9efd627f7fc
maki toda <earth.in.us@gmail.com>
2017-03-27 20:40:25 -0700

    Default project files.

---
+++ Recipe app/Recipe app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipe app
+//
+//  Created by Maki & Mai on 2017-03-27.
+//  Copyright © 2017 Maki & Mai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
