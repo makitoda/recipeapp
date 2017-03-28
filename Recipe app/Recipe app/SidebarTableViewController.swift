f4f92468fc677e338a4af46b170691a4d87313a7
maki toda <earth.in.us@gmail.com>
2017-03-28 14:13:28 -0700

    Modified SidebarTableViewController. - Added user icon image. - Modified the design of cells.

--- Recipe app/Recipe app/SidebarTableViewController.swift
+++ Recipe app/Recipe app/SidebarTableViewController.swift
@@ -10,22 +10,41 @@ import Foundation
 
 class SidebarTableViewController: UITableViewController {
     
+    var userIcon: UIImage = UIImage(named: "user")! // TODO: Set dynamic data
+    var userName: String = "John Doe"               // TODO: Set dynamic data
     var navArray = [String]()
     
     override func viewDidLoad() {
-        navArray = ["User name", "Post new recipe", "Favorite", "History"]
+        
+        navArray = ["Post new recipe",
+                    "Favorite",
+                    "History"]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2;
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return navArray.count
+        return section == 0 ? 1 : navArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.section == 0 ? 80 : 60
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SidebarTableViewControllerCell", for: indexPath)
-        
-        cell.textLabel?.text = navArray[indexPath.row]
-        
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SidebarTableViewControllerCell_UserInfo", for: indexPath)
+            cell.imageView?.image = userIcon
+            cell.textLabel?.text = userName
+            return cell
+            
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SidebarTableViewControllerCell", for: indexPath)
+            cell.textLabel?.text = navArray[indexPath.row]
+            return cell
+        }
     }
 }
