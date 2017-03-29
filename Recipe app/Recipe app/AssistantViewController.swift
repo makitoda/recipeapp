2cda7708ccae48871924a5aef3cc7daf44238eb9
maki toda <earth.in.us@gmail.com>
2017-03-28 21:10:49 -0700

    Added objects on Assistant.

---
+++ Recipe app/Recipe app/AssistantViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AssistantViewController.swift
+//  Recipe app
+//
+//  Created by Maki & Mai on 2017-03-27.
+//  Copyright © 2017 Maki & Mai. All rights reserved.
+//
+
+import Foundation
+
+class AssistantViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        if self.revealViewController() != nil {
+            
+            // Open sidebar from menuButton
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            
+            // Open sidebar by swiping screen
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            // Close sidebar by clicking main screen
+            self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        }
+    }
+}
